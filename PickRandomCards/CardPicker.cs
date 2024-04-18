20388ef646ba4d1d7dc8e3a729396e0657ff2ec1
tikil <tikil@DESKTOP-4CFOPNP>
2024-04-18 19:32:50 -0300

    Adicionar arquivos de projeto.

---
+++ PickRandomCards/CardPicker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickRandomCards
+{
+    internal class CardPicker
+    {
+        static Random Random = new Random();
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i =0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " de " + RandomSuit();
+            }
+            return pickedCards;
+        }
+        private static string RandomValue()
+        {
+            int value = Random.Next(1, 14);
+            if (value == 1) return "As";
+            if (value == 11) return "Valete";
+            if (value == 12) return "Dama";
+            if (value == 13) return "Rei";
+            return value.ToString();
+        }
+
+        private static string RandomSuit()
+        {
+            int value = Random.Next(1, 5);
+            if (value == 1) return "Espadas";
+            if (value == 2) return "Copas";
+            if (value == 3) return "Paus";
+            return "Ouros";
+        }
+    }
+}
---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PickRandomCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a quantidade de cartas a serem recebidas: ");
+            string line = Console.ReadLine();
+
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Numero invalido! Insira um numero valido!");
+            }
+        }
+    }
+}
